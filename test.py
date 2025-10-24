26b7aa8df29e030d4a6ca526ecb8d004b0bbd662
green <num110010@gmail.com>
2025-10-24 12:23:13 -0400

    param change

--- test.py
+++ test.py
@@ -11,15 +11,16 @@ import torch
 gc.collect()
 torch.cuda.empty_cache()
 
-dataset = load_countdown_dataset(batch_size=200, pairs_loaded=200)
+dataset = load_countdown_dataset(batch_size=50, pairs_loaded=50)
 
 sampler = SamplingParams(
     temperature=0.00,
+    seed=42,
     max_tokens=1024
 )
 backend = VLLMBackendMulti2(model_name="Qwen/Qwen2.5-3B-Instruct", NUM_GPUS=4, CPUS_PER_GPU=6, GPU_FRACTION_VLLM_WORKER=0.85, Sampler=sampler)
 trainer = SimpleTrainer(
-    population_size=28,
+    population_size=16,
     learning_rate=0.0005,
     seed_weight=0.001,
     backend=backend,
