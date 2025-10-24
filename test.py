10691c1c864690ec59a1ff22eef992224f923406
green <num110010@gmail.com>
2025-10-24 12:43:55 -0400

    standard tests

--- test.py
+++ test.py
@@ -20,7 +20,7 @@ sampler = SamplingParams(
 )
 backend = VLLMBackendMulti2(model_name="Qwen/Qwen2.5-3B-Instruct", NUM_GPUS=4, CPUS_PER_GPU=6, GPU_FRACTION_VLLM_WORKER=0.85, Sampler=sampler)
 trainer = SimpleTrainer(
-    population_size=16,
+    population_size=12,
     learning_rate=0.0005,
     seed_weight=0.001,
     backend=backend,
