c7812a0e9700fa82b421482b6e7bf6632abf8fba
green <num110010@gmail.com>
2025-12-13 19:40:03 -0500

    normalizedlengthreward

--- libs/datasets/postprocessreward.py
+++ libs/datasets/postprocessreward.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from typing import Callable, Dict, List, Optional, Union
 import re
+import math
 
 from libs.genome import Genome
 
@@ -44,4 +45,39 @@ class DynamicLengthReward(PostProcessReward):
                     # Incorrect answer, apply length reward (weighted average between answer reward and length reward)
                     length_reward = output_usage_proportion * self.length_reward_percent
                     genome.latest_rewards[i] = answer_reward + (1 - answer_reward) * length_reward
+
+class NormalizedLengthReward(PostProcessReward):
+    def __init__(self, length_penalty_percent: float = 0.3, length_reward_percent: float = 0.3, correct_threshold: float = 0.9):
+        self.length_penalty_percent = length_penalty_percent
+        self.length_reward_percent = length_reward_percent
+        self.correct_threshold = correct_threshold
+
+    def post_process_rewards(self, genomes: List[Genome]):
+        all_lengths = []
+        for genome in genomes:
+            for output in genome.latest_outputs:
+                all_lengths.append(len(output.split()))
+
+        mean_length = sum(all_lengths) / len(all_lengths)
+        variance = sum((x - mean_length) ** 2 for x in all_lengths) / (len(all_lengths) - 1)
+        std_dev = math.sqrt(variance)
         
+        if std_dev == 0:
+            return
+
+        for genome in genomes:
+            for i in range(len(genome.latest_outputs)):
+                output = genome.latest_outputs[i]
+                answer_reward = genome.latest_rewards[i]
+                current_len = len(output.split())
+
+                z_score = (current_len - mean_length) / std_dev
+                relative_length = 0.5 * (1 + math.erf(z_score / math.sqrt(2)))
+
+                if answer_reward > self.correct_threshold:
+                    penalty_factor = relative_length * self.length_penalty_percent
+                    genome.latest_rewards[i] = answer_reward * (1 - penalty_factor)
+                
+                else:
+                    reward_factor = relative_length * self.length_reward_percent
+                    genome.latest_rewards[i] = answer_reward + (1 - answer_reward) * reward_factor
